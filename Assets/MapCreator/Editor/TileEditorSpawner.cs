262268a56a88c13f536a4b2212bda30fd734f6d6
Xerlocked <sherlock_203b@naver.com>
2021-11-07 00:30:26 +0900

    Update TileEditorSpawner.cs
    Prototype GUI

--- Assets/MapCreator/Editor/TileEditorSpawner.cs
+++ Assets/MapCreator/Editor/TileEditorSpawner.cs
@@ -3,179 +3,126 @@ using System.Collections;
 using System.Collections.Generic;
 using Codice.Client.BaseCommands.BranchExplorer.Layout;
 using UnityEditor;
-using UnityEditor.SearchService;
 using UnityEngine;
-
-public class TileEditorSpawner : EditorWindow
+[CustomEditor(typeof(TileSpawner))]
+public class TileEditorSpawner : Editor
 {
-    private bool showLevelOptions = false;
-    private bool bPaintMode = false;
-    private int sizeLevelFloor = 1;
-    private int sizeLevelWidth = 1;
-    private int sizeLevelDepth = 1;
-    private int sizeGridHeight = 1;
-    private int sizeGridSize = 1;
-    private Transform TileSpawner;
-    
-    [MenuItem("TileEditor/ShowSpawner")]
-    static void Init()
+    private TileSpawner Spawner => (TileSpawner) target;
+    public GameObject selectObject = null;
+    private bool lockMouse = false;
+
+    public enum ToolModes
     {
-        var win = (TileEditorSpawner) GetWindow(typeof(TileEditorSpawner));
-        win.Show();
+        Move,
+        Building,
+        Drawing
     }
 
-    public void OnGUI()
-    {
-        
-        showLevelOptions = EditorGUILayout.Foldout(showLevelOptions, "Level Creator");
-        if (showLevelOptions)
-        {
-            EditorGUILayout.Space();
-            EditorGUI.indentLevel++;
-
-            EditorGUILayout.BeginHorizontal(); // Level Value
-            {
-                EditorGUILayout.BeginVertical("HelpBox");
-                EditorGUILayout.LabelField("Level Value");
-                sizeLevelFloor = EditorGUILayout.IntField("층 수", sizeLevelFloor);
-                sizeLevelWidth = EditorGUILayout.IntField("가로", sizeLevelWidth);
-                sizeLevelDepth = EditorGUILayout.IntField("세로", sizeLevelDepth);
-                EditorGUILayout.EndVertical();
-            }
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.Space();
-
-            EditorGUILayout.BeginHorizontal(); // Grid Value
-            {
-                EditorGUILayout.BeginVertical("HelpBox");
-                EditorGUILayout.LabelField("Grid Value");
-                sizeGridHeight = EditorGUILayout.IntField("층 높이", sizeGridHeight);
-                sizeGridSize = EditorGUILayout.IntField("그리드 크기", sizeGridSize);
-                EditorGUILayout.EndVertical();
-            }
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.Space();
-
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Button("New Map Spawner");
-            GUILayout.Button("Find Map Spawner");
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.Space();
-
-            bPaintMode = GUILayout.Toggle(bPaintMode, "Start Creating", "Button", GUILayout.Height(60f));
-
-            EditorGUILayout.Space();
-
-            EditorGUILayout.BeginVertical();
-
-            EditorGUILayout.LabelField("현재 레벨:");
-
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Button("<");
-            GUILayout.Button(">");
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.EndVertical();
-
-            EditorGUILayout.LabelField("선택된 타일");
-        }
-    }
-
-
-    /*
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
+    private ToolModes toolMode = ToolModes.Move;
     
-        TileSpawner map = (TileSpawner) target;
-        
-        //.GenerateMap();
-    }
-    
-    private TileSpawner _tileSpawner;
-    private int _objIndex;
-    private int _selectIndex;
-    public void OnEnable()
-    {
-        _tileSpawner = (TileSpawner) target;
-        //SceneView.duringSceneGui += SceneUpdate;
-    
-        string cursorPath = "Assets/MapCreator/Prefabs/TileCursor.prefab";
-        GameObject cursorPrefab = (GameObject) AssetDatabase.LoadAssetAtPath(cursorPath, typeof(GameObject));
-        if (cursorPrefab)
-        {
-            //_tileCursor = (GameObject) PrefabUtility.InstantiatePrefab(cursorPrefab);
-        }
-    }
-    
-    private void OnSceneGUI()
-    {
-        _tileSpawner = (TileSpawner) target;
-    
-        int controlID = GUIUtility.GetControlID(FocusType.Passive);
-        Event e = Event.current;
-        
-        Vector3 mousePos = Event.current.mousePosition;
-        //mousePos.z = Camera.current.pixelHeight - mousePos.z;
-        Ray ray = HandleUtility.GUIPointToWorldRay(mousePos);
-    
-        Debug.Log(ray.origin);
-        
-        if (Event.current.button == 0)
-        {
-            if (Event.current.type == EventType.MouseDown)
-            {
-                GUIUtility.hotControl = controlID;
-                e.Use();
-                DrawObject(true, ray.origin);
-            }
-        }
-    }
-    
-    void DrawObject(bool drag, Vector3 pos)
-    {
-        _tileSpawner = (TileSpawner) target;
-        Vector3 newPos = new Vector3(
-            Mathf.Floor(pos.x / _tileSpawner.width) * _tileSpawner.width + _tileSpawner.width / 2f,
-            0, Mathf.Floor(pos.y / _tileSpawner.height) * _tileSpawner.height + _tileSpawner.height / 2f);
-        
-        Debug.Log(newPos);
-        
-        string cursorPath = "Assets/MapCreator/Prefabs/Cube.prefab";
-        GameObject cursorPrefab = (GameObject) AssetDatabase.LoadAssetAtPath(cursorPath, typeof(GameObject));
-        GameObject createGameObject = (GameObject) PrefabUtility.InstantiatePrefab(cursorPrefab);
-        createGameObject.transform.parent = _tileSpawner.transform.parent;
-        createGameObject.transform.position = newPos;
-    }
-    
-    private void OnDisable()
-    {
-        SceneView.duringSceneGui -= SceneUpdate;
-        DestroyImmediate(_tileCursor);
-    }
-    
-    void UpdateCursorPosition(Vector3 pos)
-    {
-        _tileCursor.transform.localScale = new Vector3(1, 1, 1);
-    
-        Vector3 newPos = new Vector3(pos.x, 0, pos.z);
-        
-        _tileCursor.transform.position = newPos;
-        Debug.Log(newPos + "newPos");
-    }
-    
-    void SceneUpdate(SceneView view)
+    protected void OnSceneGUI()
     {
         Event e = Event.current;
-    
-    
+
+        Handles.BeginGUI();
+        {
+            toolMode = (ToolModes)GUI.Toolbar(new Rect(10, 10, 200, 30), (int)toolMode, new[] {"Move", "Building", "Drawing"});
+
+            if (toolMode == ToolModes.Building)
+                GUI.Window(0, new Rect(10, 70, 200, 400), BuildingPanel, "Buildings");
+        }
+        Handles.EndGUI();
+
+        if (toolMode == ToolModes.Move)
+        {
+            if (Tools.current == Tool.None)
+            {
+                Tools.current = Tool.Move;
+            }
+            return;
+        }
+
+        Tools.current = Tool.None;
+        HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+
+        if (e.type == EventType.KeyDown && e.keyCode == KeyCode.V)
+        {
+            lockMouse = !lockMouse;
+            Debug.Log("Lock State: " + lockMouse);
+        }
         
-        Debug.Log(_ray.origin);
-        
-        UpdateCursorPosition(_ray.origin);
+        if (e.type == EventType.MouseDown && e.button == 0 && toolMode == ToolModes.Building && !lockMouse)
+        {
+            if (!Select<Transform>(e))
+            {
+                Debug.Log("실행");
+                Selection.activeGameObject = Spawner.transform.gameObject;
+            }
+
+            selectObject = Selection.activeGameObject;
+        }
+
+        Selection.activeGameObject = Spawner.transform.gameObject;
     }
-    */
+
+    private void BuildingPanel(int id)
+    {
+        const int left = 10;
+        const int width = 200;
+        const int height = 400;
+        var e = Event.current;
+        
+        Vector3 activeLocalPosition = selectObject.transform.localPosition;
+        Quaternion activeLocalRotation = selectObject.transform.localRotation;
+        Vector3 activeLocalScale =  selectObject.transform.localScale;
+        Texture2D activeThumbnail = AssetPreview.GetAssetPreview(selectObject);
+        
+        GUI.DrawTexture(new Rect(width/2 - 75/2, 25, 75,75), activeThumbnail);
+        
+        EditorGUILayout.BeginVertical(new GUIStyle(
+            EditorStyles.helpBox
+            ) {margin = new RectOffset(0,0,110,0)} );
+        activeLocalPosition = EditorGUILayout.Vector3Field("Local Position", activeLocalPosition);
+        Vector3 transRotation = EditorGUILayout.Vector3Field("Local Rotation", activeLocalRotation.eulerAngles);
+        activeLocalScale = EditorGUILayout.Vector3Field("Local Scale", activeLocalScale);
+
+        EditorGUILayout.Space();
+        
+        if (GUILayout.Button("변경하기"))
+        {
+            selectObject.transform.localPosition = activeLocalPosition;
+        }
+        
+        EditorGUILayout.EndVertical();
+
+        if (e.type == EventType.MouseMove || e.type == EventType.MouseDown)
+            Repaint();
+    }
+    
+    
+    public static GameObject Select<T>(Event e) where T : UnityEngine.Component
+    {
+        Camera cam = Camera.current;
+
+        if (cam != null)
+        {
+            RaycastHit hit;
+            Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    GameObject gameObj = hit.collider.gameObject;
+                    if (gameObj.GetComponent<T>() != null)
+                    {
+                        e.Use();
+                        Selection.activeGameObject = gameObj;
+                        return gameObj;
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
