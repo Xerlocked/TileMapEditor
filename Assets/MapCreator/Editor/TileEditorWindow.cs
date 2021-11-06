7ca6ea544e3b88fd46e1ee8171f012d1e312e7e9
Xerlocked <sherlock_203b@naver.com>
2021-11-07 00:17:46 +0900

    TileEditorWindow Change
    Prototype GUI 1

--- Assets/MapCreator/Editor/TileEditorWindow.cs
+++ Assets/MapCreator/Editor/TileEditorWindow.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Codice.CM.WorkspaceServer;
+using JetBrains.Annotations;
 using UnityEditor;
 using UnityEditor.Experimental.TerrainAPI;
 using UnityEditor.UIElements;
@@ -10,7 +11,7 @@ using UnityEngine;
 public class TileEditorWindow : EditorWindow
 {
     private bool showLevelOptions = false;
-    private bool bNewTileMap = false;
+    //private bool bNewTileMap = false;
     private bool bPaintMode = false;
     private bool bEndWork = false;
     private bool canPaint = false;
@@ -33,11 +34,6 @@ public class TileEditorWindow : EditorWindow
 
     [SerializeField]
     private Vector2 cellSize = new Vector2(1f, 1f);
-    
-    private Rect rectLevelValue;
-    private Rect rectGridValue;
-
-    private Texture2D texture;
 
     private GUIStyle customGUIStyle_label;
     private GUIStyle customGUIStyle_foldout;
@@ -104,8 +100,9 @@ public class TileEditorWindow : EditorWindow
                 EditorGUILayout.Space();
                 lcGridSizeX = EditorGUILayout.FloatField("Grid Size X", lcGridSizeX);
                 EditorGUILayout.Space();
-                lcGridSizeY =  EditorGUILayout.FloatField("Grid Size Y", lcGridSizeY);
+                lcGridSizeY =  EditorGUILayout.FloatField("Grid Size Y (Z)", lcGridSizeY);
                 EditorGUILayout.Space();
+                TileSpawner = EditorGUILayout.ObjectField(TileSpawner, typeof(Transform), true) as Transform;
                 EditorGUILayout.EndVertical();
             }
             EditorGUILayout.EndHorizontal();
@@ -116,8 +113,23 @@ public class TileEditorWindow : EditorWindow
             #region 2
             
             EditorGUILayout.BeginHorizontal();
-            bNewTileMap = GUILayout.Button("새로운 맵 만들기", customGUIStyle_Button);
-            GUILayout.Button("기존 맵 이어하기", customGUIStyle_Button);
+            
+            if (GUILayout.Button("새로운 맵 만들기", customGUIStyle_Button))
+            {
+                TileSpawner = new GameObject("TileSpawner").transform;
+                canPaint = true;
+            }
+            
+            if (GUILayout.Button("기존 맵 이어하기", customGUIStyle_Button))
+            {
+                if (TileSpawner == null)
+                {
+                    Debug.LogError("TileSpawner가 설정되지 않았습니다. 설정 후 다시 시도해주세요.");
+                    return;
+                }
+                canPaint = true;
+            }
+            
             EditorGUILayout.EndHorizontal();
             EditorGUI.EndDisabledGroup(); // DisableGroup (1) 끝
             #endregion
@@ -131,8 +143,8 @@ public class TileEditorWindow : EditorWindow
             EditorGUILayout.Space();
 
             EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("현재 레벨",GUILayout.Width(100f));
-            EditorGUILayout.IntSlider(lcFloorRange, 1, lcFloor);
+            EditorGUILayout.LabelField("현재 층 수",GUILayout.Width(100f));
+            lcFloorRange = EditorGUILayout.IntSlider(lcFloorRange, 1, lcFloor);
             EditorGUILayout.EndHorizontal();
             
             EditorGUI.EndDisabledGroup(); // DisableGroup (2) 끝
@@ -157,11 +169,6 @@ public class TileEditorWindow : EditorWindow
             EditorGUI.BeginDisabledGroup(!canPaint);
             bEndWork = GUILayout.Button("작업 끝내기", customGUIStyle_Button);
             EditorGUI.EndDisabledGroup();
-            if (bNewTileMap)
-            {
-                TileSpawner = new GameObject("TileSpawner").transform;
-                canPaint = true;
-            }
 
             if (bEndWork)
             {
@@ -180,6 +187,9 @@ public class TileEditorWindow : EditorWindow
     {
         if (bPaintMode)
         {
+            cellSize.x = lcGridSizeX;
+            cellSize.y = lcGridSizeY;
+
             Vector2 cellCenter = GetSelectedCell();
             
             DisplayHelper(cellCenter);
@@ -187,6 +197,8 @@ public class TileEditorWindow : EditorWindow
             
             SceneView.RepaintAll();
         }
+
+        Cursor.visible = false;
     }
 
     private void HandleSceneViewInputs(Vector2 cellCenter)
@@ -200,12 +212,29 @@ public class TileEditorWindow : EditorWindow
             Event.current.type == EventType.MouseDown && 
             Event.current.button == 0)
         {
-            GameObject go = PrefabUtility.InstantiatePrefab(palette[paletteIndex]) as GameObject;
-            go.transform.position = new Vector3(cellCenter.x, 0, cellCenter.y);
-            go.transform.parent = TileSpawner;
+            DrawSelectPrefab(palette[paletteIndex],
+                new Vector3(cellCenter.x, FloorHeight(), cellCenter.y),
+                new Vector3(lcGridSizeX,1,lcGridSizeY),
+                TileSpawner);
         }
     }
 
+    private float FloorHeight()
+    {
+        return (lcFloorHeight * lcFloorRange) - lcFloorHeight;
+    }
+    
+    private void DrawSelectPrefab(GameObject prefab,Vector3 prefabPosition, Vector3 prefabScale, Transform prefabTransform)
+    {
+        if (prefab == null)
+            return;
+        
+        GameObject selectPrefab = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+        selectPrefab.transform.position = prefabPosition;
+        selectPrefab.transform.localScale = prefabScale;
+        selectPrefab.transform.parent = prefabTransform;
+    }
+    
     private void OnFocus()
     {
         SceneView.duringSceneGui -= this.OnSceneGUI;
@@ -240,16 +269,21 @@ public class TileEditorWindow : EditorWindow
         // Vertices of our square
         Vector3 topLeft = cellCenter + Vector2.left * cellSize * 0.5f + Vector2.up * cellSize * 0.5f;
         SwapToZero(out topLeft.z, ref topLeft.y);
+        topLeft.y = FloorHeight();
+        Debug.Log(topLeft);
 
         Vector3 topRight = cellCenter - Vector2.left * cellSize * 0.5f + Vector2.up * cellSize * 0.5f;
         SwapToZero(out topRight.z, ref topRight.y);
+        topRight.y = FloorHeight();
         
         Vector3 bottomLeft = cellCenter + Vector2.left * cellSize * 0.5f  - Vector2.up * cellSize * 0.5f;
         SwapToZero(out bottomLeft.z, ref bottomLeft.y);
+        bottomLeft.y = FloorHeight();
         
         Vector3 bottomRight = cellCenter - Vector2.left * cellSize * 0.5f - Vector2.up * cellSize * 0.5f;
         SwapToZero(out bottomRight.z, ref bottomRight.y);
-
+        bottomRight.y = FloorHeight();
+        
         // Rendering
         Handles.color = Color.green;
         Vector3[] lines = { topLeft, topRight, topRight, bottomRight, bottomRight, bottomLeft, bottomLeft, topLeft };
